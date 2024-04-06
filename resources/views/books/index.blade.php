52d619896c21e8a0fb68d2cd18651e7e7c7c8d16
funkybeppe <lucabarbero1996@gmail.com>
2024-04-06 16:17:17 +0100

    Add margin and spacing classes to form, set height 10 to input, a element and buttons

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
 <h1 class="mb-10 text-2xl">Books</h1>
 
-<form method="GET" action="{{ route('books.index') }}">
-    <input type="text" name="title" class="input" placeholder="Search by title" value="{{ request('title') }}" />
-    <button type="submit" class="btn">Search</button>
-    <a href="{{ route('books.index') }}">Clear</a>
+<form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
+    <input type="text" name="title" class="input h-10" placeholder="Search by title" value="{{ request('title') }}" />
+    <button type="submit" class="btn h-10">Search</button>
+    <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
 </form>
 
 <ul>
