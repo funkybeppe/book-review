015399c071a7bafb6b482f8732e4ed959550c7cb
funkybeppe <lucabarbero1996@gmail.com>
2024-04-07 12:59:17 +0100

    Add average rating and review count to latest filter

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -25,7 +25,7 @@ class BookController extends Controller
                 'popular_last_6months' => $books->popularLast6Months(),
                 'highest_rated_last_month' => $books->highestRatedLastMonth(),
                 'highest_rated_last_6months' => $books->highestRatedLast6Months(),
-                default => $books->latest(),
+                default => $books->latest()->withAvgRating()->withReviewsCount(),
             };
 
             $cacheKey = 'books:' . $title . ':' . $filter;
